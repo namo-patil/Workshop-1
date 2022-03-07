6f3d06e15e19476e8a24ae62b973c2385c9beb0d
namo-patil <namratapatil74738@gmail.com>
2022-03-07 11:30:52 +0530

    added UC1-start Gambler

---
+++ Gambling.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Gambling {
+    int stake = 100;
+    int bets = 1;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulation Program");
+    }
+}
