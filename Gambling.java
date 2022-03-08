9741f53eb1c71a87e6eab8c1c96827f9f515091e
namo-patil <namratapatil74738@gmail.com>
2022-03-08 14:43:03 +0530

    added program to check gambler won or lost UC-2

--- Gambling.java
+++ Gambling.java
@@ -1,9 +1,21 @@
 package com.bridgelabz;
 
 public class Gambling {
-    int stake = 100;
-    int bets = 1;
+    public static final int STAKE = 100;
+    public static final int BETS = 1;
+
+    public void winOrLoose() {
+        boolean randomCheck = (Math.random() < 0.5);
+        if (randomCheck)
+            System.out.println("Gambler is Won");
+        else
+            System.out.println("Gambler Lost");
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Program");
+        Gambling Gambler = new Gambling();
+        Gambler.winOrLoose();
     }
 }
+
