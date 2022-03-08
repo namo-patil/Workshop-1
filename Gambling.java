a8b43930749925d35c5f77ea8e0a126a37e99aa7
namo-patil <namratapatil74738@gmail.com>
2022-03-08 23:01:17 +0530

    added when will gambler resign for the day

--- Gambling.java
+++ Gambling.java
@@ -4,18 +4,36 @@ public class Gambling {
     public static final int STAKE = 100;
     public static final int BETS = 1;
 
-    public void winOrLoose() {
+    public boolean winOrLoose() {
         boolean randomCheck = (Math.random() < 0.5);
         if (randomCheck)
             System.out.println("Gambler is Won");
         else
             System.out.println("Gambler Lost");
+        return randomCheck;
+    }
+
+    public int playGame(int winLimit, int loseLimit) {
+        //winLimit and looseLimit are 50% of STAKE
+        int stake = 100;
+        while (stake < winLimit && stake > loseLimit) {
+            if (winOrLoose()) {
+                stake = stake + 1;
+                System.out.println(stake + " ");
+            } else  {
+                stake = stake - 1;
+                System.out.println(stake + " ");
+            }
+        }
+        System.out.println("Gambler would resign for the day");
+        return stake;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Program");
         Gambling Gambler = new Gambling();
         Gambler.winOrLoose();
+        Gambler.playGame(150,50);
     }
 }
 
