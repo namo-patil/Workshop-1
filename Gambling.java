58ec85670c8ff0738b4c9da02c606d50a6b0be5f
namo-patil <namratapatil74738@gmail.com>
2022-03-13 17:06:36 +0530

    added some changes in code UC-4

--- Gambling.java
+++ Gambling.java
@@ -7,19 +7,24 @@ public class Gambling {
     public static final int STAKE = 100;
     public static final int BETS = 1;
 
+    int winLimit = 150;
+    int loseLimit = 50;
+    int winCount;
+    int loseCount;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Program");
         Gambling Gambler = new Gambling();
-        Gambler.winOrLoose();
-        Gambler.gamblerResign();
-        Gambler.calTotalAmountWonOrLost();
+        //Gambler.winOrLose();
+        //Gambler.gamblerResign();
+        Gambler.calTotalAmountWonOrLostIn20Days();
     }
 
     /* UC2- As a Gambler make $1 bet so either win or
     loose $1
      */
-    public boolean winOrLoose() {
-        boolean randomCheck = (Math.random() < 0.5);
+    public boolean winOrLose() {
+        boolean randomCheck = (Math.random() > 0.5);
         if (randomCheck)
             System.out.println("Gambler is Won");
         else
@@ -33,43 +38,54 @@ public class Gambling {
      */
     public int gamblerResign() {
         int stake = 100;
-        int winLimit = 150;
-        int looseLimit = 50;
-        while (stake < winLimit && stake > looseLimit) {
-            if (winOrLoose()) {
+        while (stake < winLimit && stake > loseLimit) {
+            if (winOrLose()) {
                 stake = stake + 1;
                 System.out.println(stake + " ");
+                winCount++;
             } else {
                 stake = stake - 1;
                 System.out.println(stake + " ");
+                loseCount++;
             }
         }
-        System.out.println("Gambler has resigned for the day.");
+        System.out.println("Stake : " + stake + " ");
+        System.out.println(" 'GAMBLER RESIGNED FOR THE DAY.' ");
+        System.out.println();
         return stake;
     }
 
     /* UC4- After 20 days of playing every day would like to know
         the total amount won or lost.
      */
-    public void calTotalAmountWonOrLost() {
+    public void calTotalAmountWonOrLostIn20Days() {
         int num_Playing_Days = 20;
-        int winLimit = 150;
-        int looseLimit = 50;
-        int winCount = 0;
-        int loseCount = 0;
-        for (int day = 0; day < num_Playing_Days; day++) {
-            int count = gamblerResign();
-            if (count == winLimit) {
-                winCount++;
+        int winCount_20days = 0;
+        int  loseCount_20days = 0;
+        int countCheck = 0;
+
+        for (int day = 1; day <= num_Playing_Days; day++) {
+            countCheck = gamblerResign();
+
+            if (countCheck == winLimit) {
+                winCount_20days++;
             } else {
-                loseCount++;
+                loseCount_20days++;
             }
+            System.out.println("===*****=====*****=====Day " + day + " Done=====*****=====*****===");
         }
-        int totalAmountWon = +winCount;
-        int totalAmountLost = +loseCount;
+        System.out.println();
         System.out.println("=====After 20 Days of playing every day=====");
-        System.out.println("Gambler Won Total Amount: $" + totalAmountWon);
-        System.out.println("Gambler Lost Total Amount: $" + totalAmountLost);
+        System.out.println("Gambler Won Days : " + winCount_20days);
+        System.out.println("Gambler Lost Days : " + loseCount_20days);
+        System.out.println();
+        int totalAmountWon = winCount;
+        int totalAmountLost = loseCount;
+        System.out.println("Total amount Won: " + totalAmountWon);
+        System.out.println("Total amount Lost: " + totalAmountLost);
     }
 }
+
+
+
 
